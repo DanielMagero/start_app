e1e6a9e2bcdeab7865666c2348bd2376aa7d01f9
DanielMagero <mdmagero700@gmail.com>
2024-11-06 08:53:34 +0300

    update

--- app/page.js
+++ app/page.js
@@ -3,13 +3,16 @@ import Content from "../components/Content";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  function goToNextPage() {}
   return (
     <div>
-      <Title heading="Tuesday" />
-      <Title heading="Wednesday" />
+      <Title heading="Tuesday" onClick />
       <Content details="Beginnings" />
-      <Content king="life" />
       <Footer />
+      <br />
+
+      <Title heading="Wednesday" />
+      <Content king="life" />
       <Footer />
     </div>
   );
