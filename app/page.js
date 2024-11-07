22862e196a8dff0321225a87e6be988b65599fcc
DanielMagero <mdmagero700@gmail.com>
2024-11-07 09:12:18 +0300

    learning buttons

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Title from "../components/Title";
 import Content from "../components/Content";
 import Footer from "../components/Footer";
+import Button from "@mui/material/Button";
 
 export default function Home() {
   function goToNextPage() {}
@@ -9,16 +10,19 @@ export default function Home() {
     <div>
       <Title heading="Tuesday" onClick={goToNextPage} />
       <Content details="Beginnings" />
+      <Button variant="contained">Contained</Button>
       <Footer />
       <br />
 
       <Title heading="Wednesday" />
       <Content king="life" />
+      <Button variant="text">Submit</Button>
       <Footer />
       <br />
 
       <Title heading="Thursday" />
       <Content king="milds" />
+      <Button variant="outlined">GO!</Button>
       <Footer />
     </div>
   );
